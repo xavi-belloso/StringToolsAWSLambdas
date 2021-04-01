f341f9e3d6167534349237a218af95fa8aa1283e
Xavi Belloso <xbelloso@voxelgroup.net>
2021-04-01 09:48:16 +0200

    first commit

---
+++ acceptanceTests.cmd
@@ -0,0 +1,2 @@
+curl --header "Content-Type: application/json" --request POST --data "{\"source\":\"xavi\"}" https://2u7v8fmob8.execute-api.eu-west-1.amazonaws.com/demo/api/isPalindrome
+curl --header "Content-Type: application/json" --request POST --data "{\"source\":\"xaax\"}" https://2u7v8fmob8.execute-api.eu-west-1.amazonaws.com/demo/api/isPalindrome
---
+++ src/IsPalindrome.cs
@@ -0,0 +1,40 @@
+﻿using Amazon.Lambda.APIGatewayEvents;
+using Amazon.Lambda.Core;
+using Newtonsoft.Json;
+using System;
+using System.Threading.Tasks;
+
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+namespace StringTools
+{
+    public class IsPalindrome
+    {
+        public async Task<APIGatewayProxyResponse> Handler(APIGatewayProxyRequest lambdaEvent, ILambdaContext context)
+        {
+            context.Logger.LogLine($"Body {lambdaEvent.Body}.");
+            var source = JsonConvert.DeserializeObject<Root>(lambdaEvent.Body).Source;
+            context.Logger.LogLine($"Source {source}.");
+
+            var reverseArray = source.ToCharArray();
+            Array.Reverse(reverseArray);
+
+            var reverse = new string(reverseArray);
+            context.Logger.LogLine($"Source {reverse}.");
+
+            var isPalindrome = source == reverse;
+        
+            return new APIGatewayProxyResponse
+            {
+                Body = isPalindrome ? $"{source} is a Palindrome" : $"{source} is not a Palindrome",
+                StatusCode = 200
+            };
+
+        }
+    }
+
+    public class Root
+    {
+        [JsonProperty("source")]
+        public string Source { get; set; }
+    }
+}
